afdf2b50cdffd0bb246c53def7599a1d95d56225
Arsen <109721927+estremenyo@users.noreply.github.com>
2024-02-23 14:07:21 +0400

    Add full DOM functionality

--- script.js
+++ script.js
@@ -21,76 +21,76 @@ const Model = (function() {
     })();
     const getBoard = () => { return board };
     const getPlayer = (player) => { return players[player] };
-     return {getBoard, getPlayer};
+    return {getBoard, getPlayer};
 })();
 
-
 const View = (function() {
-    console.log("Hello! Execute Controller.playGame() to start playing!");
-    const displayBoard = (function() {
-        console.log(" ");
-        // Print Model rows (outer loop) and columns (inner loop)
-        for (let i = 0; i < 3; i++) {
-            let rowToPrint = "";
-            for (let j = 0; j < 3; j++) {
-                rowToPrint += `${Model.getBoard()[i][j]} `;
-            }
-            console.log(rowToPrint);
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            let div = document.createElement("div");
+            div.dataset.row = i;
+            div.dataset.col = j;
+            document.querySelector("main").appendChild(div);
         }
-    });
-    return {displayBoard};
+    }
+    function updateCell(cell, player) {
+        cell.textContent = player === 1 ? "X" : "O";
+    }
+    return { updateCell };
 })();
 
 
 const Controller = (function() {
-    // const updateBoard;
-    // const checkCell;
     const updateTurn = function() {
         Model.getPlayer(1).isTurn = Model.getPlayer(1).isTurn ? false : true;
         Model.getPlayer(2).isTurn = Model.getPlayer(2).isTurn ? false : true;
     }
     
+    const initializeCells = (function() {
+        let divNodeList = document.querySelectorAll("div");
+        let divArray = Array.from(divNodeList);
+        divArray.forEach(div => addEventListener("click", handleClick));
+    })();
+
+    function removeEventListeners() {
+        let divNodeList = document.querySelectorAll("div");
+        let divArray = Array.from(divNodeList);
+        divArray.forEach(div => removeEventListener("click", handleClick));
+    }
+
+    function handleClick(e) {
+        if (e.target.textContent === "") {
+            let currentPlayerTurn = Model.getPlayer(1).isTurn ? 1 : 2;
+            View.updateCell(e.target, currentPlayerTurn);
+            updateBoard(currentPlayerTurn, e.target);
+            if (checkWinner()) {
+                removeEventListeners();
+                if (Model.isTie) alert("It's a tie");
+                else alert(`Player ${currentPlayerTurn} has won`);
+                return;
+            }
+            updateTurn();
+        } else {
+            return;
+        }
+    }
+
     // assume we have already validated input earlier in the call stack
     const updateBoard = function(player, location) {
-        location = location.split(", ");
         let value;
-        if (player === 1) value = "x";
-        else if (player === 2) value = "y";
-        let [row, col] = location;
+        if (player === 1) value = "X";
+        else if (player === 2) value = "O";
+
+        let row = location.dataset.row;
+        let col = location.dataset.col;
         Model.getBoard()[row][col] = value;
-    }
-
-    const getInput = function(currentPlayer) {
-        let answer = "";
-        while (!validateInput(answer)) {
-            answer = prompt(`Player ${currentPlayer}, enter your answer here like so: 1, 2`);
-        }
-        return answer;
-    }
-
-    // return true if the answer is valid
-    const validateInput = function(answer) {
-        let parsed = answer.split(", ");
-        if (parsed === undefined || parsed.length !== 2) {
-            return false;
-        }
-        if (!(["0", "1", "2"].includes(parsed[0])) || !(["0", "1", "2"].includes(parsed[1]))) {
-            console.log("Out of bounds");
-            return false;
-        }
-        let board = Model.getBoard();
-        if (board[parsed[0]][parsed[1]] !== undefined) {
-            console.log("Cell already occupied");
-            return false;
-        }
-        return true;
     }
 
     // checks for horizontal, vertical, and diagonal victory or a tie
     const checkWinner = function() {
         let board = Model.getBoard();
-        let player1Symbol = "x";
-        let player2Symbol = "y";
+        let player1Symbol = "X";
+        let player2Symbol = "O";
         let player1Matched = 0;
         let player2Matched = 0;
         let toMatch = 3;
@@ -150,34 +150,15 @@ const Controller = (function() {
             return row.every(cell => cell !== undefined);
         })
         if (isTie) {
-            View.isTie = true;
+            Model.isTie = true;
             return true;
         }
-        // if none of the above checks have returned true, no one has won the game yet, so conitnue
+        // if none of the above checks have returned true, no one has won the game yet, so continue
         return false;
     }
 
-    const playRound = function() {
-        View.displayBoard();
-        let currentPlayerTurn = Model.getPlayer(1).isTurn ? 1 : 2;
-        let playerAnswer = getInput(currentPlayerTurn);
-        updateBoard(currentPlayerTurn, playerAnswer);
-        updateTurn();
-    }
 
-    const playGame = function() {
-        // Set player 1 to go first
-        Model.getPlayer(1).isTurn = true;
-        // Keep playing rounds until winner found
-        while (!checkWinner()) {
-            playRound();
-        }   
-        // Display message when game over
-        View.displayBoard();
-        console.log(`Game Over!`);
-        if (View.isTie) console.log("It's a tie!");
-        else console.log(`${Model.getPlayer(1).hasWon ? "Player One Won" : "Player Two Won"}`);
-    }
-
-    return {playGame, checkWinner}; // remove checkWinner in production
+    return {};
 })();
+
+Model.getPlayer(1).isTurn = true;
